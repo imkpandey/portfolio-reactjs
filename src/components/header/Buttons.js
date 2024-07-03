e40c8e8803dc08092b1fad8efe4e13b3a79e3153
Kartikey Pandey <imkpandey31@gmail.com>
2024-07-03 13:51:26 +0530

    Updated Resume

--- src/components/header/Buttons.js
+++ src/components/header/Buttons.js
@@ -3,7 +3,12 @@ import React from "react";
 const Buttons = () => {
   return (
     <div className="buttons">
-      <a href="../../resume.pdf" className="btn" download>
+      <a
+        href="https://drive.google.com/file/d/1GTz2wLt2WSv4vXLN31e-oi_aZgCIKREf/view?usp=drive_link"
+        className="btn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Download CV
       </a>
       <a href="#contact" className="btn">
