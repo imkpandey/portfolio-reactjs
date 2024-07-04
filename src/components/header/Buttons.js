ce710e09f4daf1ec92826fce2e0ca59df2f1c34f
Kartikey Pandey <imkpandey31@gmail.com>
2024-07-04 14:58:21 +0530

    Updated resume link

--- src/components/header/Buttons.js
+++ src/components/header/Buttons.js
@@ -4,7 +4,7 @@ const Buttons = () => {
   return (
     <div className="buttons">
       <a
-        href="https://drive.google.com/file/d/1GTz2wLt2WSv4vXLN31e-oi_aZgCIKREf/view?usp=drive_link"
+        href="https://drive.google.com/file/d/1Sw8AU2Sb6SbOxV2UpRMXD094ZaTnXsW6/view?usp=drive_link"
         className="btn"
         target="_blank"
         rel="noopener noreferrer"
